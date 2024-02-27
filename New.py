c750645ee86774d26f9c7ac514ac555517b9b378
Dinith Pamoda <dinithpgurusinghe830@gmail.com>
2024-02-27 23:39:52 +0530

    First Commit
    First Date: 27/02/2024

---
+++ New.py
@@ -0,0 +1,28 @@
+import phonenumbers
+from phonenumbers import geocoder
+from Test import number
+import folium
+
+key = "c81b79cd419a45d98c54779333e0773d"
+
+check_number = phonenumbers.parse(number)
+number_location = geocoder.description_for_number(check_number, "en")
+print(number_location)
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+geocoder = OpenCageGeocode(key)
+
+query = str(number_location)
+results = geocoder.geocode(query)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(lat,lng)
+
+map_location = folium.Map(location = [lat,lng], zoom_start=9)
+folium.Marker([lat,lng], popup=number_location).add_to(map_location)
+map_location.save("Mylocation.html")
